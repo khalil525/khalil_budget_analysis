0b82dddd43e61d647f38fef0e0c859d7924db814
khalil525 <49661975+khalil525@users.noreply.github.com>
2023-08-23 15:07:41 -0400

    Update Budget_Analysis-app.py

--- Budget_Analysis-app.py
+++ Budget_Analysis-app.py
@@ -29,7 +29,7 @@ st.set_page_config(page_title = "Budget Analysis",
 
 # ---- ROW A ---- 
 
-st.markdown('### Lets talk MONEY :bar:') 
+st.markdown('### Lets talk MONEY $$$') 
 
 col1, col2, col3 = st.columns(3)
 
